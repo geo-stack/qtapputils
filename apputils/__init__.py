a94b5d51adfa2e7c8bb7c339745dee5bd059384d
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-03-06 11:47:16 -0500

    Create __init__.py

---
+++ apputils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © AppUtils Project Contributors
+# https://github.com/jnsebgosselin/apputils
+#
+# This file is part of AppUtils.
+# Licensed under the terms of the MIT License.
+# -----------------------------------------------------------------------------
