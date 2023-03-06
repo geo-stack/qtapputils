2fdae4da9ba3193ba75fadda6632c4c2f3054291
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-03-06 11:56:42 -0500

    Update __init__.py

--- apputils/__init__.py
+++ apputils/__init__.py
@@ -6,3 +6,8 @@
 # This file is part of AppUtils.
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
+
+version_info = (0, 1, 0, 'dev0')
+__version__ = '.'.join(map(str, version_info))
+__date__ = '06/03/2023'
+__project_url__ = "https://github.com/jnsebgosselin/apputils"
