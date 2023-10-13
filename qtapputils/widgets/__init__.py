11afdf0ec4439b685e981e87a6be12f86c2ef23a
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-10-13 15:21:12 -0400

    Expose taskmanagers classes in widgets module

--- qtapputils/widgets/__init__.py
+++ qtapputils/widgets/__init__.py
@@ -8,3 +8,4 @@
 # -----------------------------------------------------------------------------
 from .range import RangeSpinBox, RangeWidget
 from .waitingspinner import WaitingSpinner
+from .taskmanagers import WorkerBase, TaskManagerBase, LIFOTaskManager
