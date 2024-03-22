c82d6516a8c3f417b535567051f94c4277418952
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2024-03-22 13:31:01 -0400

    Bump version to 0.5.1 for release

--- qtapputils/__init__.py
+++ qtapputils/__init__.py
@@ -7,7 +7,7 @@
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
 
-version_info = (0, 5, 0)
+version_info = (0, 5, 1)
 __version__ = '.'.join(map(str, version_info))
-__date__ = '19/01/2024'
+__date__ = '22/03/2024'
 __project_url__ = "https://github.com/jnsebgosselin/qtapputils"
