e448f0c7a1e3d280bffb0858c37671f860fee0f1
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2024-04-23 09:21:56 -0400

    Update __init__.py

--- qtapputils/__init__.py
+++ qtapputils/__init__.py
@@ -7,7 +7,7 @@
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
 
-version_info = (0, 5, 1)
+version_info = (0, 5, 2)
 __version__ = '.'.join(map(str, version_info))
-__date__ = '22/03/2024'
+__date__ = '23/04/2024'
 __project_url__ = "https://github.com/jnsebgosselin/qtapputils"
