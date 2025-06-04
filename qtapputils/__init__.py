30d3659e57c42ec67c7f68cab8af18ad1ed48bca
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-06-04 11:32:49 -0400

    Update __init__.py

--- qtapputils/__init__.py
+++ qtapputils/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
 # Copyright © QtAppUtils Project Contributors
-# https://github.com/jnsebgosselin/apputils
+# https://github.com/geo-stack/qtapputils
 #
 # This file is part of QtAppUtils.
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
 
-version_info = (0, 8, 0)
+version_info = (0, 8, 1)
 __version__ = '.'.join(map(str, version_info))
-__date__ = '17/01/2025'
-__project_url__ = "https://github.com/jnsebgosselin/qtapputils"
+__date__ = '04/06/2025'
+__project_url__ = "https://github.com/geo-stack/qtapputils"
