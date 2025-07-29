b2c9e15ab1b2e019597dd602026f00968f9fcfff
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-07-29 15:13:33 -0400

    Update __init__.py

--- qtapputils/__init__.py
+++ qtapputils/__init__.py
@@ -7,7 +7,7 @@
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
 
-version_info = (0, 8, 1)
+version_info = (0, 8, 2)
 __version__ = '.'.join(map(str, version_info))
-__date__ = '04/06/2025'
+__date__ = '29/07/2025'
 __project_url__ = "https://github.com/geo-stack/qtapputils"
