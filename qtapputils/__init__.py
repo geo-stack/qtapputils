30f1d9c571e1fe5b810f2fe23b8f6d8bee659be9
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-10-07 13:14:41 -0400

    Bump version to 0.9.3 for release

--- qtapputils/__init__.py
+++ qtapputils/__init__.py
@@ -7,7 +7,7 @@
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
 
-version_info = (0, 9, 2)
+version_info = (0, 9, 3)
 __version__ = '.'.join(map(str, version_info))
-__date__ = '25/09/2025'
+__date__ = '07/10/2025'
 __project_url__ = "https://github.com/geo-stack/qtapputils"
