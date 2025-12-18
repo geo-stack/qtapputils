0ce8f6c9b4e0e2e80f3ae34752873184dd1de212
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2025-12-18 13:18:51 -0500

    Update version info for release

--- qtapputils/__init__.py
+++ qtapputils/__init__.py
@@ -7,7 +7,7 @@
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
 
-version_info = (0, 9, 3)
+version_info = (0, 10, 0)
 __version__ = '.'.join(map(str, version_info))
-__date__ = '07/10/2025'
+__date__ = '18/12/2025'
 __project_url__ = "https://github.com/geo-stack/qtapputils"
